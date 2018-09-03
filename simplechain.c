62a7377f45edc1c67d19e7a47bdea5a11fb2263d
jennifer wasson <wasson@hoare7.cs.umsl.edu>
2018-09-02 21:40:02 -0500

    v1.5

--- simplechain.c
+++ simplechain.c
@@ -1,3 +1,13 @@
+//Jennifer Wasson
+//Operating Systems
+//Project 1
+//Source used:
+//https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html
+
+
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,9 +18,11 @@ void helpoptions()
 {
   printf("options for running Project 1:  \n");
   printf("type ./proj1 -h for help options\n");
-  printf("type ./proj1 -p to enable perror messages.\n");
+  printf("type ./proj1 -p to generate test perror message.\n");
   printf("type ./proj1 -n x to run fork loop for x number of times.\n");
 }
+
+
 int main(int argc, char *argv[])
 {
   pid_t childpid = 0;
@@ -43,7 +55,7 @@ int main(int argc, char *argv[])
         xvalue = optarg;
         break;
       case '?':
-        if (optopt == 'c')
+        if (optopt == 'n')
           fprintf (stderr, "Option -%c requires an argument.\n", optopt);
         else if (isprint (optopt))
           fprintf (stderr, "Unknown option `-%c'.\n", optopt);
@@ -63,10 +75,7 @@ int main(int argc, char *argv[])
   for (index = optind; index < argc; index++)
     printf ("Non-option argument %s\n", argv[index]);
                    
- // if(xvalue != NULL){
-
-   //n = atoi(xvalue);
-  //}
+ 
   
 
   if(hflag == 1)
@@ -78,10 +87,18 @@ int main(int argc, char *argv[])
    if(xvalue != NULL){
 
      n = atoi(xvalue);
+     if (n == 0){
+       printf("Error:  non-numerical data entered\n");
+       return 1;}
   }
    //make the error string for perror
    char str1[50];
    char str2[13];
+   //check user input for executable
+   if(strlen(argv[0]) > 20){
+     printf("%s","Executable name too long");
+     return 1;
+   }
    strcpy(str1, argv[0]);
    strcpy(str2, " : Error: ");
    if(strlen(str2)  < 14)
@@ -90,6 +107,7 @@ int main(int argc, char *argv[])
   if(pflag == 1)
   { 
 
+   printf("Testing perror with fopen:  \n");
     
    FILE *fp;  
    fp = fopen("file.txt", "r");
@@ -109,14 +127,26 @@ int main(int argc, char *argv[])
     if (childpid == -1)
     {
        
-       perror(str1);       
-       return 1;
-     }
-     if(childpid)
+      perror(str1);       
+      return 1;
+    }
+    if(childpid)
        break;
-   }
+  }
+   //sleep(10);
    
+   //int nchars  = atoi(argv[3]);
+   //char mybuf[100];
+   //char ch;
+   //for (i = 0; i < nchars; i++){
+    //scanf(" %c", &ch);
+    //mybuf[i] = ch;
+    //}
+  // mybuf[i] = '\0';
+   
+//   fprintf(stderr, "process ID: %ld %s\n", (long)getpid(), mybuf);
    fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-       i, (long)getpid(), (long)getppid(), (long)childpid);
+      i, (long)getpid(), (long)getppid(), (long)childpid);
+   //sleep(m);
   return 0;
 }
